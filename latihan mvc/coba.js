a638d6989575a3ad0a5f32ac112ed8d36665d064
Adzka <adzka2008@gmail.com>
2024-06-05 14:51:12 +0700

    tambah gitignore

--- latihan mvc/coba.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-a = '1997-04-02'
-b = 'abcd'
-c = '20-20-2000'
-
-dateRegex = /(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[0-1])/;
-console.log(dateRegex.test(a))
-console.log(dateRegex.test(b))
-console.log(typeof path.join(__dirname, 'connect'))
