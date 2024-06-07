f2e96e70f40745923eb6f850892f2e8be10c1cc4
Adzka <adzka2008@gmail.com>
2024-06-07 09:33:58 +0700

    tambah style di tiap halaman

--- web/c19-2.js
+++ web/c19-2.js
@@ -7,17 +7,17 @@ const biodata = JSON.parse(readFileSync('biodata.json', 'utf-8'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    console.log('masuk load /');
+    // console.log('masuk load /');
     res.render('index', { biodata })
 })
 
 app.get('/add', (req, res) => {
-    console.log('masuk get /add');
+    // console.log('masuk get /add');
     res.render('form', { biodata: {}, id: -1 })
 })
 
 app.post('/add', (req, res) => {
-    console.log('masuk post /');
+    // console.log('masuk post /');
     let body = '';
     req.on('data', (chunk) => body += chunk);
     req.on('close', () => {
@@ -35,13 +35,13 @@ app.post('/add', (req, res) => {
 
 app.get('/edit/:id', (req, res) => {
     let id = Number(req.params.id);
-    console.log('masuk get /edit', id);
-    console.log(biodata[id]);
+    // console.log('masuk get /edit', id);
+    // console.log(biodata[id]);
     res.render('form', { biodata, id });
 })
 
 app.post('/edit/:id', (req, res) => {
-    console.log('masuk post /edit')
+    // console.log('masuk post /edit')
     let id = Number(req.params.id);
     let body = '';
     req.on('data', (chunk) => body += chunk);
@@ -53,7 +53,7 @@ app.post('/edit/:id', (req, res) => {
         let married = JSON.parse(params.get('married'));
         let birthdate = params.get('birthdate');
         biodata[id] = { name, height, weight, married, birthdate };
-        console.log(biodata[id]);
+        // console.log(biodata[id]);
         writeFileSync('biodata.json', JSON.stringify(biodata))
         res.redirect('/')
     })
@@ -61,7 +61,7 @@ app.post('/edit/:id', (req, res) => {
 
 app.get('/delete/:id', (req, res) => {
     try {
-        console.log('masuk /delete', req.params.id)
+        // console.log('masuk /delete', req.params.id)
         let id = Number(req.params.id);
         biodata.splice(id, 1);
         writeFileSync('biodata.json', JSON.stringify(biodata))
--- web/layout.js
+++
@@ -1,8 +0,0 @@
-const { readFileSync } = require('node:fs')
-const html = readFileSync('index.html', 'utf-8')
-
-function layout(content) {
-    return html.replace('#content#', content)
-}
-
-module.exports = { layout }
