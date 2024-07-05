465737d49e30fcc33091ccd9d9b00391f45b5daa
Adzka <adzka2008@gmail.com>
2024-07-05 09:29:27 +0700

    ubah router todos jadi tanpa menggunakan model

--- c22/routes/todos.js
+++ c22/routes/todos.js
@@ -2,49 +2,55 @@ var express = require('express');
 const Todo = require('../models/Todo');
 var router = express.Router();
 
-router.get('/', async function (req, res, next) {
-    try {
-        const data = await Todo.readTodo(req.query);
-        res.status(200).json({ data, total, pages, page: req.query.page, limit: req.query.limit })
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-});
+module.exports = (db) => {
+    const todos = db.collection('todos');
+    router.get('/', async function (req, res, next) {
+        try {
+            const total = await todos.find({ $and: [{ executor: req.executor }, { title: `/${req.title}/` }, { deadline: { $lt: endDeadline, $gt: startDeadline } }, { complete: req.complete },] });
+            const pages = Math.ceil(total / req.limit);
+            const data = await todos.find({ $and: [{ executor: req.executor }, { title: `/${req.title}/` }, { deadline: { $lt: endDeadline, $gt: startDeadline } }, { complete: req.complete },] }, { _id: 1, title: 1, complete: 1, deadline: 1, executor: 1 }).sort({ _id: -1 }).limit(req.limit).skip((req.page - 1) * req.limit).toArray();
+            res.status(200).json({ data, total, pages, page: req.page, limit: req.limit })
+        } catch (error) {
+            res.status(500).json({ message: error.message })
+        }
+    });
 
-router.get('/:id', async function (req, res, next) {
-    try {
-        const data = await Todo.getTodo(req.params.id);
-        res.status(200).json({ data })
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-});
+    router.get('/:id', async function (req, res, next) {
+        try {
+            const data = await todos.find({ _id: req.params.id }, { _id: 1, title: 1, complete: 1, deadline: 1, executor: 1 }).toArray();
+            res.status(200).json({ data });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    });
 
-router.post('/', async function (req, res, next) {
-    try {
-        const data = await Todo.addTodo(req.body);
-        res.status(201).json({ data })
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-});
+    router.post('/', async function (req, res, next) {
+        try {
+            await todos.insertOne({ title: req.title, executor: req.executor });
+            const data = await todos.find({}, { _id: 1, title: 1, complete: 1, deadline: 1, executor: 1 }).sort({ _id: -1 }).limit(1);
+            res.status(201).json({ data });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    });
 
-router.put('/:id', async function (req, res, next) {
-    try {
-        const data = await Todo.updateTodo(req.params.id);
-        res.status(201).json({ data })
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-});
+    router.put('/:id', async function (req, res, next) {
+        try {
+            await todos.updateMany({ _id: req.params.id }, { title: req.title, deadline: req.deadline, complete: req.complete });
+            const data = await todos.find({ _id: req.params.id }, { _id: 1, title: 1, complete: 1, deadline: 1, executor: 1 });
+            res.status(201).json({ data });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    });
 
-router.delete('/:id', async function (req, res, next) {
-    try {
-        const data = await Todo.removeTodo(req.params.id);
-        res.status(200).json({ data })
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-});
-
-module.exports = router;
+    router.delete('/:id', async function (req, res, next) {
+        try {
+            const data = await todos.find({ _id: req.params.id }, { _id: 1, title: 1, complete: 1, deadline: 1, executor: 1 });
+            await users.deleteMany({ _id: req.params.id });
+            res.status(200).json({ data });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    });
+};
