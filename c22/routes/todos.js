e809f52bd11ac2efc00641a6847eec642023fb50
Adzka <adzka2008@gmail.com>
2024-07-09 11:12:20 +0700

    melengkapi API

--- c22/routes/todos.js
+++ c22/routes/todos.js
@@ -1,15 +1,30 @@
 var express = require('express');
-const Todo = require('../models/Todo');
 var router = express.Router();
+var ObjectId = require('mongodb').ObjectId;
 
 module.exports = (db) => {
     const todos = db.collection('todos');
     router.get('/', async function (req, res, next) {
         try {
-            const total = await todos.find({ $and: [{ executor: req.executor }, { title: `/${req.title}/` }, { deadline: { $lt: endDeadline, $gt: startDeadline } }, { complete: req.complete },] });
-            const pages = Math.ceil(total / req.limit);
-            const data = await todos.find({ $and: [{ executor: req.executor }, { title: `/${req.title}/` }, { deadline: { $lt: endDeadline, $gt: startDeadline } }, { complete: req.complete },] }, { _id: 1, title: 1, complete: 1, deadline: 1, executor: 1 }).sort({ _id: -1 }).limit(req.limit).skip((req.page - 1) * req.limit).toArray();
-            res.status(200).json({ data, total, pages, page: req.page, limit: req.limit })
+            req.query.limit = Number(req.query.limit) || 5;
+            req.query.page = Number(req.query.page) || 1;
+            req.query.title = req.query.title || '';
+            req.query.sortBy = req.query.sortBy || '_id';
+            req.query.sortMode = req.query.sortMode || 'desc';
+            let query = [];
+            query.push({ executor: req.query.executor });
+            req.query.title ? query.push({ title: new RegExp(req.query.title) }) : '';
+            if (req.query.endDateDeadline && req.query.startDateDeadline) query.push({ deadline: { $lt: req.query.endDateDeadline, $gt: req.query.startDateDeadline } })
+            else if (req.query.endDateDeadline) query.push({ deadline: { $lt: req.query.endDateDeadline } })
+            else if (req.query.startDateDeadline) query.push({ deadline: { $gt: req.query.startDateDeadline } });
+            typeof req.query.complete !== 'undefined' ? query.push({ complete: JSON.parse(req.query.complete) }) : '';
+            let sort = `{"${req.query.sortBy}":${req.query.sortMode == 'desc' ? -1 : 1}}`;
+            console.log(sort);
+            console.log(query);
+            const total = await todos.count({ $and: query });
+            const pages = Math.ceil(total / req.query.limit);
+            const data = await todos.find({ $and: query }, { _id: 1, title: 1, complete: 1, deadline: 1, executor: 1 }).sort(JSON.parse(sort)).limit(req.query.limit).skip((req.query.page - 1) * req.query.limit).toArray();
+            res.status(200).json({ data, total, pages, page: req.query.page, limit: req.query.limit })
         } catch (error) {
             res.status(500).json({ message: error.message })
         }
@@ -17,8 +32,8 @@ module.exports = (db) => {
 
     router.get('/:id', async function (req, res, next) {
         try {
-            const data = await todos.find({ _id: req.params.id }, { _id: 1, title: 1, complete: 1, deadline: 1, executor: 1 }).toArray();
-            res.status(200).json({ data });
+            const data = await todos.findOne({ _id: new ObjectId(req.params.id) }, { _id: 1, title: 1, complete: 1, deadline: 1, executor: 1 });
+            res.status(200).json(data);
         } catch (error) {
             res.status(500).json({ message: error.message });
         }
@@ -26,9 +41,9 @@ module.exports = (db) => {
 
     router.post('/', async function (req, res, next) {
         try {
-            await todos.insertOne({ title: req.title, executor: req.executor });
-            const data = await todos.find({}, { _id: 1, title: 1, complete: 1, deadline: 1, executor: 1 }).sort({ _id: -1 }).limit(1);
-            res.status(201).json({ data });
+            await todos.insertOne({ title: req.body.title, executor: req.body.executor });
+            const data = await todos.find().sort({ _id: -1 }).limit(1).toArray();
+            res.status(201).json(data[0]);
         } catch (error) {
             res.status(500).json({ message: error.message });
         }
@@ -36,9 +51,9 @@ module.exports = (db) => {
 
     router.put('/:id', async function (req, res, next) {
         try {
-            await todos.updateMany({ _id: req.params.id }, { title: req.title, deadline: req.deadline, complete: req.complete });
-            const data = await todos.find({ _id: req.params.id }, { _id: 1, title: 1, complete: 1, deadline: 1, executor: 1 });
-            res.status(201).json({ data });
+            await todos.updateOne({ _id: new ObjectId(req.params.id) }, { title: req.body.title, deadline: req.body.deadline, complete: req.body.complete });
+            const data = await todos.findOne({ _id: new ObjectId(req.params.id) }, { _id: 1, title: 1, complete: 1, deadline: 1, executor: 1 });
+            res.status(201).json(data);
         } catch (error) {
             res.status(500).json({ message: error.message });
         }
@@ -46,11 +61,13 @@ module.exports = (db) => {
 
     router.delete('/:id', async function (req, res, next) {
         try {
-            const data = await todos.find({ _id: req.params.id }, { _id: 1, title: 1, complete: 1, deadline: 1, executor: 1 });
-            await users.deleteMany({ _id: req.params.id });
-            res.status(200).json({ data });
+            const data = await todos.findOne({ _id: new ObjectId(req.params.id) }, { _id: 1, title: 1, complete: 1, deadline: 1, executor: 1 });
+            await users.deleteOne({ _id: new ObjectId(req.params.id) });
+            res.status(200).json(data);
         } catch (error) {
             res.status(500).json({ message: error.message });
         }
     });
+
+    return router;
 };
--- c22/routes/users.js
+++ c22/routes/users.js
@@ -1,15 +1,23 @@
 var express = require('express');
-const User = require('../models/User');
 var router = express.Router();
+var ObjectId = require('mongodb').ObjectId;
 
 module.exports = (db) => {
   const users = db.collection('users');
   router.get('/', async function (req, res, next) {
     try {
-      const total = await users.count({ name: `/${req.query}/` });
-      const pages = Math.ceil(total / req.limit);
-      const data = await users.find({ name: `/${req.query}/` }, { _id: 1, name: 1, phone: 1 }).sort({ _id: -1 }).limit(req.limit).skip((req.page - 1) * req.limit).toArray();
-      res.status(200).json({ data, total, pages, page: req.page, limit: req.limit });
+      req.query.limit = Number(req.query.limit) || 5;
+      req.query.query = req.query.query || '';
+      req.query.page = req.query.page || 1;
+      req.query.sortBy = req.query.sortBy || '_id';
+      req.query.sortMode = req.query.sortMode || 'desc';
+      const strSort = `{"${req.query.sortBy}":${req.query.sortMode === 'desc' ? -1 : 1}}`;
+      // console.log(strSort);
+      const offset = (req.query.page - 1) * req.query.limit;
+      const data = await users.find({ name: new RegExp(req.query.query, 'i') }, { "_id": 1, name: 1, phone: 1 }).sort(JSON.parse(strSort)).limit(req.query.limit).skip(offset).toArray();
+      const total = await users.count();
+      const pages = Math.ceil(total / req.query.limit);
+      res.status(200).json({ data, total, pages, page: req.query.page, limit: req.query.limit, offset });
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
@@ -17,8 +25,8 @@ module.exports = (db) => {
 
   router.get('/:id', async function (req, res, next) {
     try {
-      const data = await users.find({ _id: req.params.id }, { _id: 1, name: 1, phone: 1 });
-      res.status(200).json({ data });
+      const data = await users.findOne({ _id: new ObjectId(req.params.id) }, { _id: 1, name: 1, phone: 1 });
+      res.status(200).json(data);
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
@@ -26,9 +34,9 @@ module.exports = (db) => {
 
   router.post('/', async function (req, res, next) {
     try {
-      await users.insertOne({ name: req.name, phone: req.phone });
-      const data = user.find({}, { _id: 1, name: 1, phone: 1 }).sort({ _id: -1 }).limit(1);
-      res.status(201).json({ data });
+      await users.insertOne({ name: req.body.name, phone: req.body.phone });
+      const data = await users.find().sort({ _id: -1 }).limit(1).toArray();
+      res.status(201).json(data[0]);
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
@@ -36,9 +44,9 @@ module.exports = (db) => {
 
   router.put('/:id', async function (req, res, next) {
     try {
-      await users.updateMany({ _id: req.params.id }, { $set: { name: req.name, phone: req.phone } });
-      const data = await users.find({ _id: req.params.id }, { _id: 1, name: 1, phone: 1 });
-      res.status(201).json({ data });
+      await users.updateOne({ _id: new ObjectId(req.params.id) }, { $set: { name: req.body.name, phone: req.body.phone } });
+      const data = await users.findOne({ _id: new ObjectId(req.params.id) }, { _id: 1, name: 1, phone: 1 });
+      res.status(201).json(data);
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
@@ -46,11 +54,13 @@ module.exports = (db) => {
 
   router.delete('/:id', async function (req, res, next) {
     try {
-      const data = await users.find({ _id: req.params.id }, { _id: 1, name: 1, phone: 1 });
-      await users.deleteMany({ _id: req.params.id });
-      res.status(200).json({ data });
+      const data = await users.findOne({ _id: new ObjectId(req.params.id) }, { _id: 1, name: 1, phone: 1 });
+      await users.deleteOne({ _id: new ObjectId(req.params.id) });
+      res.status(200).json(data);
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
   });
+
+  return router;
 }
